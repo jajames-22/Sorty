53cd0055fdd89f0267667ab682ed95cb40a34292
jajames-22 <rojasjamesbenedict@gmail.com>
2025-11-19 21:17:57 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/sorty/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.sorty
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.sorty.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var bind: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bind.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(bind.root.id)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
